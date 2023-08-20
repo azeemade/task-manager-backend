579cafbfd37c21ec8adf2f5809cb840ac982d2f9
Azeem Adenuga <adenugaazeem80@gmail.com>
2023-08-21 00:41:11 +0100

    update to app blade

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,7 +15,7 @@
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
 
     <!-- Scripts -->
-    {{-- @vite(['resources/sass/app.scss', 'resources/js/app.js']) --}}
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
 
 <body>
@@ -81,7 +81,7 @@
             @yield('content')
         </main>
     </div>
-    <script src="{{ mix('/js/app.js') }}"></script>
+    {{-- <script src="{{ mix('/js/app.js') }}"></script> --}}
 </body>
 
 </html>
