7e74edf955598bd3e0ff832b9fd266ceacae8ec8
Azeem Adenuga <adenugaazeem80@gmail.com>
2023-08-20 10:52:39 +0100

    another update

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,6 +17,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/clear-cache', function () {
+    Artisan::call('optimize:clear');
+    return "Cache is cleared";
 });
 
 Route::get('/api-all', function () {
